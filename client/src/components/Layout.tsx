686c985c6ab818b63412cdba88a150df5143d583
Jose <romerojosemanuel@hotmail.com>
2025-05-15 18:51:10 -0600

    feat(client): add UserButton component in Menu

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const Layout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen grid grid-cols-1 place-content-between bg-zinc-100">
+    <div className="min-h-screen grid grid-cols-1 place-content-between bg-white">
       <Menu />
       <main className="sm:w-96 md:w-2xl mx-auto">{children}</main>
       footer
--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,24 +1,29 @@
 import { NavLink } from "react-router-dom"
-import { SignOutButton, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { SignedIn, UserButton } from '@clerk/clerk-react';
 
 const Menu = () => {
-  return (
-    <nav className='p-4'>
-      <div className='md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3'>
-        <div className='flex gap-2 items-center'>Urusai</div>
-        <div className='flex gap-x-5 items-center'>
 
+  return (
+    <nav className='p-6'>
+      <div className='md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3'>
+        <div className='flex gap-x-5 items-center'>
           <SignedIn>
             <NavLink className="!text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" to={"/"}>Home</NavLink>
             <NavLink className="!text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" to={"/learning"}>Learning</NavLink>
-            <SignOutButton redirectUrl="/sign-in">
-              <button className="focus:outline-none text-white bg-red-700 hover:cursor-pointer hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign Out</button>
-            </SignOutButton>
           </SignedIn>
-
-          <SignedOut>
-            <NavLink className="!text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none" to="/sign-in">Sign in</NavLink>
-          </SignedOut>
+        </div>
+        <div className='flex gap-2 items-center'>Urusai</div>
+        <div className="flex">
+          <SignedIn>
+            <UserButton
+              showName={true} 
+              appearance={{ 
+                elements: { 
+                  userButtonBox: "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5"
+                }
+              }}
+            />
+          </SignedIn>
         </div>
       </div>
     </nav>
