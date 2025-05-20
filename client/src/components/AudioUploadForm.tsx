81da7f8327f820dffb55ede41bec3f8fd32e1690
Jose <romerojosemanuel@hotmail.com>
2025-05-19 19:43:18 -0600

    feat(client): add AudioUploadForm component

---
+++ client/src/components/AudioUploadForm.tsx
@@ -0,0 +1,61 @@
+import { ChangeEvent, FormEventHandler } from "react";
+import SoundRecorder from "./SoundRecorder";
+
+interface Props {
+  handleSubmit: FormEventHandler
+  handleAudio(e: ChangeEvent<HTMLInputElement>): void
+  handleRecordAudio(audioBlob: Blob): void
+  updateAudioTitle(title: string): void
+  updateIsRecordAudio(isRecordAudio: boolean): void
+  isRecordAudio: boolean
+}
+
+const AudioUploadForm = ({ handleSubmit, handleAudio, handleRecordAudio, updateAudioTitle, updateIsRecordAudio, isRecordAudio }: Props) => {
+  
+  return (
+    <form className="rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:px-8" onSubmit={handleSubmit}>
+      <div className="mb-5">
+        <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Audio title</label>
+        <input 
+          type="text"
+          id="title"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="Example: Hanging out with friends in the park"
+          onChange={ (e) => updateAudioTitle(e.target.value) }
+          required
+        />
+      </div>
+
+      { !isRecordAudio ? (
+        <div className="mb-5">
+          <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="audio">Load audio</label>
+          <input
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none p-2.5"
+            id="audio"
+            type="file"
+            accept="audio/mp3,audio/ogg,audio/wav"
+            onChange={(e) => handleAudio(e)}
+            aria-describedby="upload_audio"
+            required
+          />
+        </div>
+      ) : (
+          <SoundRecorder handleRecordAudio={handleRecordAudio} />
+      ) }
+
+      <div className="mb-5">
+        <button
+          type="button"
+          className="text-sm text-blue-600 hover:cursor-pointer flex w-full justify-end"
+          onClick={() => updateIsRecordAudio(!isRecordAudio)}
+        >
+          { isRecordAudio ? <>or load audio</> : <>or record audio</>}
+        </button>
+      </div>
+
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">upload Audio</button>
+    </form>
+  )
+}
+
+export default AudioUploadForm
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,10 +3,10 @@ import { useAuth } from '@clerk/clerk-react';
 import { uploadAudio, getAudios } from '../services/audioService';
 import { Audio } from '../types/Audio';
 import ShowAudios from '../components/ShowAudios';
-import SoundRecorder from '../components/SoundRecorder';
 import Notification from '../components/Notification';
 import { notificationMessageDefault } from '../consts/notificationMessageDefault';
 import { NotificationMessage } from '../types/NotificationMessage';
+import AudioUploadForm from '../components/AudioUploadForm';
 
 const HomePage = () => {
   const { getToken, userId } = useAuth();
@@ -108,62 +108,30 @@ const HomePage = () => {
     setFile(myFile);
   }
 
+  const updateAudioTitle = (title: string) => setAudioTitle(title);
+
+  const updateIsRecordAudio = (isRecordAudio: boolean) => setIsRecordAudio(isRecordAudio);
+
   return (
     <div className='flex flex-col gap-16'>
-      <form className="rounded-2xl bg-white px-4 py-10 shadow-md ring-1 ring-black/5 sm:px-8" onSubmit={handleSubmit}>
-        <div className="mb-5">
-          <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900">Audio title</label>
-          <input 
-            type="text"
-            id="title"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            placeholder="Example: Hanging out with friends in the park"
-            onChange={ (e) => setAudioTitle(e.target.value) }
-            required
-          />
-        </div>
-
-        { !isRecordAudio ? (
-          <div className="mb-5">
-            <label className="block mb-2 text-sm font-medium text-gray-900" htmlFor="audio">Load audio</label>
-            <input
-              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none p-2.5"
-              id="audio"
-              type="file"
-              accept="audio/mp3,audio/ogg,audio/wav"
-              onChange={(e) => handleAudio(e)}
-              aria-describedby="upload_audio"
-              required
-            />
-          </div>
-        ) : (
-          <SoundRecorder handleRecordAudio={handleRecordAudio} />
-        ) }
-
-        <div className="mb-5">
-          <button
-            type="button"
-            className="text-sm text-blue-600 hover:cursor-pointer flex w-full justify-end"
-            onClick={() => setIsRecordAudio(!isRecordAudio)}
-          >
-            { isRecordAudio ? <>or load audio</> : <>or record audio</>}
-          </button>
-        </div>
-
-        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">upload Audio</button>
-      </form>
-
+      <AudioUploadForm
+        handleSubmit={handleSubmit}
+        handleAudio={handleAudio}
+        handleRecordAudio={handleRecordAudio}
+        updateAudioTitle={updateAudioTitle}
+        updateIsRecordAudio={updateIsRecordAudio}
+        isRecordAudio={isRecordAudio}
+        
+      />
       <ShowAudios
         title={"My Audios"}
         audios={myAudios}
         isLoaded={isLoaded}
       />
-
       <Notification
         message={notificationMessage}
         notificationClose={ () => setNotificationMessage(notificationMessageDefault) }
       />
-
     </div>
   )
 }
