395ed018deca8ee43284df4a384eca5ee4393140
Jose <romerojosemanuel@hotmail.com>
2025-05-20 15:47:24 -0600

    feat(client): update Menu component

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -5,21 +5,19 @@ const Menu = () => {
 
   return (
     <nav className='p-6'>
-      <div className='md:w-3/4 mx-auto flex items-center md:justify-between gap-x-3'>
-        <div className='flex gap-x-5 items-center'>
+      <div className='xl:w-3/4 mx-auto flex items-center md:justify-between gap-x-1'>
+        <div className='flex gap-1 items-center font-medium text-sm md:text-xl px-5 py-2.5'>ウルサイ</div>
+        <div className='flex gap-1 md:gap-x-5 items-center [&_*]:font-medium [&_*]:md:!text-xl'>
           <SignedIn>
-            <NavLink className="!text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" to={"/"}>Home</NavLink>
-            <NavLink className="!text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" to={"/learning"}>Learning</NavLink>
-          </SignedIn>
-        </div>
-        <div className='flex gap-2 items-center'>Urusai</div>
-        <div className="flex">
-          <SignedIn>
+            <NavLink className="!text-gray-900 bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-lg text-sm px-5 py-2.5" to={"/"}>Home</NavLink>
+            <NavLink className="!text-gray-900 bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-lg text-sm px-5 py-2.5" to={"/learning"}>Learning</NavLink>
             <UserButton
               showName={true} 
               appearance={{ 
                 elements: { 
-                  userButtonBox: "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5"
+                  userButtonBox: "px-5 py-2.5 flex !gap-5 bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-lg",
+                  userButtonOuterIdentifier: "hidden md:block",
+                  
                 }
               }}
             />
