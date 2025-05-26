7f3e8981f8ef75c683e1abe77837f920686fda3c
Jose <romerojosemanuel@hotmail.com>
2025-05-25 18:12:53 -0600

    feat(client): add typewriter

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom"
 import { SignedIn, UserButton } from '@clerk/clerk-react';
+import { Typewriter } from "react-simple-typewriter";
 
 const Menu = () => {
 
   return (
     <nav className='p-6'>
       <div className='xl:w-3/4 mx-auto flex items-center md:justify-between gap-x-1'>
-        <div className='flex gap-1 items-center font-medium text-sm md:text-xl px-5 py-2.5'>ウルサイ</div>
+        <div className='flex gap-1 items-center font-medium text-sm md:text-xl px-5 py-2.5 min-w-25'>
+          <Typewriter words={["ウルサイ", "Noisy", "Ruidoso"]} loop={false} cursor={true} />
+        </div>
         <div className='flex gap-1 md:gap-x-5 items-center [&_*]:font-medium [&_*]:md:!text-xl'>
           <SignedIn>
             <NavLink className="!text-gray-900 bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 rounded-lg text-sm px-5 py-2.5" to={"/home"}>Home</NavLink>
