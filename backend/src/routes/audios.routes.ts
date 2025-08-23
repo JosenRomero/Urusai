46f104bfaad2a116d996ad8fcae9e87d78396f8a
Jose <romerojosemanuel@hotmail.com>
2025-08-22 18:17:12 -0600

    refactor(backend): audios.controller file

--- backend/src/routes/audios.routes.ts
+++ backend/src/routes/audios.routes.ts
@@ -1,55 +1,57 @@
 import { Router } from "express";
 import { requireAuth } from '@clerk/express'
-import audiosController from '../controllers/audios.controller';
+import * as audiosController from '../controllers/audios.controller';
+import * as audioController from '../controllers/audio.controller';
+import * as userController from '../controllers/user.controller';
+import * as commentController from '../controllers/comments.controller';
+import * as favoritesController from '../controllers/favorites.controller';
+import * as followController from '../controllers/follow.controller';
+import * as likeController from '../controllers/like.controller';
 
 import multer from '../config/multerConfig';
 
 const router = Router();
 
-router.get("/unauthorized", audiosController.unauthorized);
+router.post("/clerk-webhook", userController.handleUser);
 
-router.post("/clerk-webhook", audiosController.handleUser);
-
-router.get("/profile/:profileUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getProfile);
+router.get("/profile/:profileUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), userController.getProfile);
 
 router.get("/all-audios", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getAllAudios);
 
-router.get("/favorite-audios", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getFavoriteAudios);
+router.get("/favorite-audios", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), favoritesController.getFavoriteAudios);
 
 router.get("/my-audios/userId/:userId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getAudios);
 
-router.post("/uploadAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), audiosController.uploadAudio);
+router.post("/uploadAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), audioController.uploadAudio);
 
-router.post("/uploadAndAnalyzeAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), audiosController.uploadAndAnalyzeAudio);
+router.post("/uploadAndAnalyzeAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), audioController.uploadAndAnalyzeAudio);
 
-router.post("/follow/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.addFollow);
+router.post("/follow/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), followController.addFollow);
 
-router.delete("/unFollow/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.unFollow);
+router.delete("/unFollow/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), followController.unFollow);
 
-router.get("/followers/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getFollowers);
+router.get("/followers/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), followController.getFollowers);
 
-router.get("/followings/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getFollowings);
+router.get("/followings/:targetUserId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), followController.getFollowings);
 
-router.put("/:audioId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.updateAudio);
+router.delete("/:audioId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audioController.deleteAudio);
 
-router.delete("/:audioId", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.deleteAudio);
+router.post("/:audioId/like/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), likeController.addLike);
 
-router.post("/:audioId/like/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.addLike);
+router.delete("/:audioId/dislike/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), likeController.removeLike);
 
-router.delete("/:audioId/dislike/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.removeLike);
+router.post("/:audioId/favorite/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), favoritesController.addFavorite);
 
-router.post("/:audioId/favorite/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.addFavorite);
+router.delete("/:audioId/removeFav/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), favoritesController.removeFavorite);
 
-router.delete("/:audioId/removeFav/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.removeFavorite);
+router.get("/:audioId/comments", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), commentController.getComments);
 
-router.get("/:audioId/comments", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getComments);
+router.post("/:audioId/comment", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), commentController.addComment);
 
-router.post("/:audioId/comment", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), multer.single('audio'), audiosController.addComment);
+router.delete("/:audioId/removeComment", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), commentController.removeComment);
 
-router.delete("/:audioId/removeComment", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.removeComment);
+router.get("/:audioId/infoAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audioController.getInfoAudio);
 
-router.get("/:audioId/infoAudio", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getInfoAudio);
-
-router.get("/:audioId/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audiosController.getAudio);
+router.get("/:audioId/:audioType", requireAuth({ signInUrl: process.env.CLERK_SIGN_IN_URL }), audioController.getAudio);
 
 export default router;
